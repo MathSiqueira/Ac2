1a42634c5c0c1df1226dcd96ac5fdaf406205757
MathSiqueira <math.siqueira12@gmail.com>
2019-09-03 21:26:41 -0300

    AC2 TAREFA

--- LOJA_VIRTUAL/urls.py
+++ LOJA_VIRTUAL/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('paginas/', include('paginas.urls')),
+    path('', include('paginas.urls')),
     path('admin/', admin.site.urls),
 ]
